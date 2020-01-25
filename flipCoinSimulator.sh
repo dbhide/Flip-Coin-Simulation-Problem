1f8065d1c6a494c9dceec7de016d5188637ef989
dbhide <dhanashree.bhide3@gmail.com>
2020-01-25 10:55:25 +0530

    ADDED: Stored result in Dictionary and calculated percentage of Singlet Combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,29 @@
 #!/bin/bash -x
 
+read -p "How many times you want to flip coin? " flipCount
+
+declare -A coin
+
+counter=1;
 coinCheck=1;
+heads=0;
+tails=0;
 
-coinFlip=$((RANDOM%2));
+while [ $counter -le $flipCount ]
+do
+	coinFlip=$((RANDOM%2));
+   if [ $coinCheck -eq $coinFlip ]
+   then
+      coin[H]=$((++heads))
+      echo "Heads!!"
+   else
+      coin[T]=$((++tails))
+      echo "Tails!!"
+   fi
+   ((counter++))
+done
 
-if [ $coinCheck -eq $coinFlip ]
-then
-	echo "Heads!!"
-else
-	echo "Tails!!"
-fi
+percentHead=$((coin[H]*100/$flipCount))
+percentTail=$((coin[T]*100/$flipCount))
+
+echo "FlipCoin " ${coin[@]}
